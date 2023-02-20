ff73cc3bfda7058e46a14149370e1f4e0743a46e
Knut Kirkhorn <knutkirk@hotmail.com>
2023-02-20 18:48:32 +0100

    Use tab indentation

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-import querystring from 'querystring';
-import {exec} from 'child_process';
+import querystring from 'node:querystring';
+import {exec} from 'node:child_process';
 
 export default function openGoogle(search) {
-    // eslint-disable-next-line no-param-reassign
-    search = querystring.escape(search);
+	// eslint-disable-next-line no-param-reassign
+	search = querystring.escape(search);
 
-    // Default command for linux
-    let command = 'xdg-open';
+	// Default command for linux
+	let command = 'xdg-open';
 
-    if (process.platform === 'win32') {
-        command = 'start';
-    } else if (process.platform === 'darwin') {
-        command = 'open';
-    }
+	if (process.platform === 'win32') {
+		command = 'start';
+	} else if (process.platform === 'darwin') {
+		command = 'open';
+	}
 
-    exec(`${command} https://www.google.com/search?q=${search}`, (err, stdout, stderr) => {
-        if (err || stderr) {
-            throw new Error('Could not open Google');
-        }
-    });
+	exec(`${command} https://www.google.com/search?q=${search}`, (error, stdout, stderr) => {
+		if (error || stderr) {
+			throw new Error('Could not open Google');
+		}
+	});
 }
