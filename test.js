31a367ad11e7589aa1e8e5be510f2320ca0ce281
Knut Kirkhorn <knutkirk@hotmail.com>
2023-02-20 18:51:03 +0100

    Fix lint errors

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ import test from 'ava';
 import openGoogle from './index.js';
 
 test('dont throw exception', t => {
-    t.notThrows(() => {
-        openGoogle('Knut Kirkhorn');
-    });
+	t.notThrows(() => {
+		openGoogle('Knut Kirkhorn');
+	});
 });
